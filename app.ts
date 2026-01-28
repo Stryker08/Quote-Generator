b569b593dbea48a78cbc63bbc5ef887fd4986798
Stryker <stryker.shaw@fhnw.edu>
2026-01-28 10:11:27 -0700

    fix: Added cors

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (request, response) => {
      response.send("Welcome to the Quote Server!")
